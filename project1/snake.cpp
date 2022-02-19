5c09b42e08690b59e75f67f06ab2867aa2ac4498
Najam Ali Abbas <90936436+Ctoic@users.noreply.github.com>
2022-02-19 16:39:30 +0000

    Add files via upload

---
+++ project1/snake.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int a=5;
+    int sum;
+    b
+    sum=a+12;
+    cout<<sum<<endl;
+
+    cout<<"hello world ";
+    return 0;
+
+}
+
+  
